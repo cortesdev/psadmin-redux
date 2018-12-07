21c976047fbb169a880e8a982d0a78ad091acd5b
Ricardo Cortes dos Santos <ricardo.cortesdossantos@diconium.com>
2018-12-07 17:55:23 +0100

    Async redux store dispatch error

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -4,64 +4,30 @@ import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
-    // Constructor
-
-    constructor(props, context) {
+     constructor(props, context) {
         super(props, context);
-
-        this.state = {
-            course: { title: "" }
-        };
-
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-
-    // Child Functions
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course});
-    }
-
-    onClickSave() {
-        this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index) {
         return <div key={index}>{course.title}</div>;
     }
 
-    // Render function
     render() {
         return (
-            // TODO move to child component
-            <div>
+             <div>
                 <h1>Courses</h1>
                 {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input 
-                    type="text"
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title}/>
-
-                <input 
-                    type="submit"
-                    value="Save"
-                    onClick={this.onClickSave}/>
             </div>
         );
     }
 }
 
-//Proptypes validation
 CoursesPage.propTypes = {
     courses: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired
 };
  
 
-// Redux connect and related functions
 function mapStateToProps(state, ownProps) {
     return {
         courses: state.courses
@@ -74,5 +40,4 @@ function mapDispatchToProps(dispatch) {
     };
 }
 
-// export page
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
