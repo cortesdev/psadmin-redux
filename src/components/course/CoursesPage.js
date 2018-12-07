2b0275453eab6e8f484e8030c805826c16a4a7f0
Ricardo Cortes dos Santos <ricardo.cortesdossantos@diconium.com>
2018-12-07 11:35:56 +0100

    Add courses Page initial markup

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,13 @@
+import React, {PropTypes} from 'react';
+ 
+class CoursesPage extends React.Component {
+    render() {
+        return (
+            <nav>
+                <h1>Courses</h1>
+            </nav>
+        );
+    }
+}
+
+export default CoursesPage;
